3d0e7567907e5abe0fe30c0b506b021354456f12
Latoya Rhoden <rhoden_latoya@yahoo.com>
2019-10-17 13:08:58 -0500

    Add an X or O to a square when clicked

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,9 @@ var gamebox;
 var boards;
 var statuses;
 var eachboards;
+var startTurn;
+var attempt;
+var state = new Array();
 
 window.onload = function() {
     boards = document.querySelector("#board");
@@ -9,9 +12,23 @@ window.onload = function() {
     for (var i = 0; i < eachboards.length; i++) {
         eachboards[i].classList.add('square');
     }
-    
-    statuses = document.querySelector("#status");
-    statuses.addEventListener("click", function(){
-        
-    });
+    for (var i = 0; i < eachboards.length; i++){
+        eachboards[i].addEventListener("click", playMove);          
+    }
+    function playMove(click){
+        var attempt = document.getElementsByClassName("square")
+        startTurn = prompt("Choose Your Move", "Type X or O").toUpperCase();
+        switch (startTurn) {
+            case "X":
+                this.textContent= "X";
+                state.push("X");
+                break;
+            case "O":
+                this.textContent = "O";
+                state.push("O");
+                break;
+            default:
+                break;
+        }
+    }   
 }

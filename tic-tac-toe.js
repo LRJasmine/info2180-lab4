e35236910b372365507fb80888c0285daa494141
Latoya Rhoden <rhoden_latoya@yahoo.com>
2019-10-18 14:57:57 -0500

    Check winner and update status

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -9,6 +9,7 @@ var state = new Array();
 window.onload = function() {
     boards = document.querySelector("#board");
     eachboards = boards.querySelectorAll("div");
+    statuses = document.querySelector("#status");
     for (var i = 0; i < eachboards.length; i++) {
         eachboards[i].classList.add("square");
     }
@@ -23,22 +24,36 @@ window.onload = function() {
             case "X":
                 this.textContent= "X";
                 state.push("X");
+                winner(statuses);
                 break;
             case "O":
                 this.textContent = "O";
                 state.push("O");
+                winner(statuses);
                 break;
             default:
-                break;
+        
         }
     }
-
     function hoverColour(mouseover){
         this.classList.add("hover");
     }
     function hoverColour2(mouseleave){
         this.classList.remove("hover");
     }
-        
-            
+                 
+}
+
+function winner(statuses){
+    state.sort();
+    var resultO = state.filter(i => i === "O").length;
+    var resultX = state.filter(i => i === "X").length;
+    
+    if (resultO >= 3 && resultO > resultX){
+        statuses.classList.add("you-won");
+        statuses.textContent = "Congratulations! O is the Winner!";
+    }else if (resultX >= 3 && resultX > resultO){
+        statuses.classList.add("you-won");
+        statuses.textContent = "Congratulations! X is the Winner!";
+    }
 }
